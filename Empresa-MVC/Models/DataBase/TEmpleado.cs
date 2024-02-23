5519b54875cf1bfe17854e2cf4b4a0785b5a5a77
Oscar Ivan Carreto Martinez <oscar.carmaiv@outlook.com>
2024-02-23 16:02:28 -0600

    FEAT: Agrega los modelos de la BD

---
+++ Empresa-MVC/Models/DataBase/TEmpleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Empresa_MVC.Models.DataBase
+{
+    public partial class TEmpleado
+    {
+        public int IdNumEmp { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellidos { get; set; } = null!;
+        public int Edad { get; set; }
+        public int Puesto { get; set; }
+        public string Estatus { get; set; } = null!;
+
+        public virtual TCatPuesto PuestoNavigation { get; set; } = null!;
+    }
+}
