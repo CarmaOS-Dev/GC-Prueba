20cc082f2ce7ff9c3d8e1de3b6933852c5f1a0fb
Oscar Ivan Carreto Martinez <oscar.carmaiv@outlook.com>
2024-02-23 15:59:16 -0600

    FEAT: Agrega el Proyecto MVC

---
+++ Empresa-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Empresa_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
